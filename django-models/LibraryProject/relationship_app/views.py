c23116d6f6c195d8faefc9adc8ca9243e1d00272
otakukim <kimberlymjungwa@gmail.com>
2025-11-09 22:03:02 +0200

    Refactor views.py for clarity and imports

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
 from .models import Book
 from .models import Library
-
+from django.views.generic.detail import DetailView
 # Function-based view to list all books
 def list_books(request):
     books = Book.objects.all()
@@ -17,6 +17,7 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
 
 
 
