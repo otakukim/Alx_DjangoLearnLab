2652399da5c080a0d9f55db5511167dd10afe3c8
otakukim <kimberlymjungwa@gmail.com>
2025-11-09 22:25:25 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -53,6 +53,19 @@ urlpatterns = [
 ]
 
 
+from django.shortcuts import render, get_object_or_404
+from .models import Book, Library
+from django.views.generic import DetailView
+
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
 
 
 
