2f0f499ca2f3ab3f8c9dd737fa8181ccf189c65e
otakukim <kimberlymjungwa@gmail.com>
2025-11-16 20:41:03 +0200

    Update admin.py

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -18,3 +18,26 @@ class CustomUserAdmin(UserAdmin):
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
+from django.contrib.auth.models import Group, Permission
+from advanced_features_and_security.models import Book
+from django.contrib.contenttypes.models import ContentType
+
+# Get ContentType for Book
+book_ct = ContentType.objects.get_for_model(Book)
+
+# Get permissions
+can_view = Permission.objects.get(codename="can_view", content_type=book_ct)
+can_create = Permission.objects.get(codename="can_create", content_type=book_ct)
+can_edit = Permission.objects.get(codename="can_edit", content_type=book_ct)
+can_delete = Permission.objects.get(codename="can_delete", content_type=book_ct)
+
+# Create groups
+viewers = Group.objects.create(name="Viewers")
+editors = Group.objects.create(name="Editors")
+admins = Group.objects.create(name="Admins")
+
+# Assign permissions
+viewers.permissions.add(can_view)
+editors.permissions.add(can_view, can_create, can_edit)
+admins.permissions.add(can_view, can_create, can_edit, can_delete)
+
