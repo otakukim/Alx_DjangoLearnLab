e4b1b0c9e5c12e136389044142883fb77bea3f0f
otakukim <kimberlymjungwa@gmail.com>
2025-11-16 18:51:54 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,22 +2,15 @@ from django.shortcuts import render
 from .models import Book, Library
 from django.views.generic import DetailView
 
-
-# --------------------------
-# Function-Based View (FBV)
-# --------------------------
 def list_books(request):
     books = Book.objects.all()
-    return render(request, "list_books.html", {"books": books})
+    return render(request, "relationship_app/list_books.html", {"books": books})
 
-
-# --------------------------
-# Class-Based View (CBV)
-# --------------------------
 class LibraryDetailView(DetailView):
     model = Library
     template_name = "library_detail.html"
     context_object_name = "library"
+
 
 
 
