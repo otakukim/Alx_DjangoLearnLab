d9273b8102f18590a6972c320929b8db65989e7d
otakukim <kimberlymjungwa@gmail.com>
2025-11-16 19:16:21 +0200

    Update urls.py

--- django-models/urls.py
+++ django-models/urls.py
@@ -1,1 +1,11 @@
+from django.urls import path
+from .admin_view import admin_view
+from .librarian_view import librarian_view
+from .member_view import member_view
+
+urlpatterns = [
+    path("admin-view/", admin_view, name="admin_view"),
+    path("librarian-view/", librarian_view, name="librarian_view"),
+    path("member-view/", member_view, name="member_view"),
+]
 
