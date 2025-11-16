59c42da1d08bf342604382e2d36cba0fcec8390d
otakukim <kimberlymjungwa@gmail.com>
2025-11-16 20:06:53 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -11,6 +11,33 @@ class LibraryDetailView(DetailView):
     template_name = "relationship_app/library_detail.html"  # <-- Checker requires this exact text
     context_object_name = "library"
 
+from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
+
+# Admin
+def is_admin(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Admin"
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, "relationship_app/admin_view.html")
+
+# Librarian
+def is_librarian(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Librarian"
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, "relationship_app/librarian_view.html")
+
+# Member
+def is_member(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Member"
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, "relationship_app/member_view.html")
+
 
 
 
