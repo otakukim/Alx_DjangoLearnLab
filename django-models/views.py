d9174582eaf15475740b9b730db15b4eefedb4b3
otakukim <kimberlymjungwa@gmail.com>
2025-11-16 19:13:07 +0200

    Update views.py

--- django-models/views.py
+++ django-models/views.py
@@ -1,1 +1,13 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
+from .models import UserProfile
+
+
+def is_admin(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Admin"
+
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, "admin_view.html")
 
