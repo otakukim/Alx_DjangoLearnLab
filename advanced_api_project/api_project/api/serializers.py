a6a0266eda2ddcdc092271109a34cf9a39ae7e8e
otakukim <kimberlymjungwa@gmail.com>
2025-11-30 21:22:20 +0200

    Update serializers.py

--- advanced_api_project/api_project/api/serializers.py
+++ advanced_api_project/api_project/api/serializers.py
@@ -1,6 +1,7 @@
 from .models import Book
 from rest_framework import serializers
 from django.http import JsonResponse
+from .models import Author, Book
 
 def home(request):
     return JsonResponse({"message": "Welcome to the Book API!"})
@@ -9,3 +10,8 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ['id', 'title', 'author']
+        
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = '__all__'
