af95abd01e60b7d83fd3ef6decab37b0dbdea965
otakukim <kimberlymjungwa@gmail.com>
2025-11-30 21:10:40 +0200

    Update urls.py

--- advanced-api-project/api_project/api/urls.py
+++ advanced-api-project/api_project/api/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path ,include
-from .views import BookListCreateAPIView, BookViewSet,home
+from .views import BookListCreateAPIView, BookViewSet,homeBookListView,
+    BookDetailView,
+    BookCreateView,
+    BookUpdateView,
+    BookDeleteView
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken.views import obtain_auth_token
 
@@ -11,4 +15,10 @@ urlpatterns = [
     path('books/', BookListCreateAPIView.as_view(), name='book-list'),
     path('', include(router.urls)),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),  # token retrieval
+    path('books/', BookListView.as_view(), name='book-list'),
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
+    path('books/create/', BookCreateView.as_view(), name='book-create'),
+    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
+    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
+
     ]
