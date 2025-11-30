f4b4e73a924af4b60c3a1c61bf6c0ffaaac6a5ba
otakukim <kimberlymjungwa@gmail.com>
2025-11-30 21:19:57 +0200

    Update models.py

--- advanced_api_project/api_project/api/models.py
+++ advanced_api_project/api_project/api/models.py
@@ -6,6 +6,11 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     published_date = models.DateField()
+    
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField(blank=True, null=True)
 
 def __str__(self):
+
     return self.title
