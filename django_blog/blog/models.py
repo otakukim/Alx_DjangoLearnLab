e52df3df17f36e4ea45d7e68450cca6aac9d571a
otakukim <kimberlymjungwa@gmail.com>
2025-12-04 11:16:41 +0200

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
@@ -8,4 +9,5 @@ class Post(models.Model):
 	author: models.ForeignKey
 
 def __str__(self):
+
     return self.title
