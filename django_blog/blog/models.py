deb7d2fb458d5245a7ab632f8432994c1183ff91
otakukim <kimberlymjungwa@gmail.com>
2025-12-04 10:07:38 +0200

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Post(model.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    content = models.CharField(max_length=200)
+    published_date = models.IntegerField()
