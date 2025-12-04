8589a3f1c68db8fb4b163e016dd78322f472c493
otakukim <kimberlymjungwa@gmail.com>
2025-12-04 11:25:18 +0200

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,12 +1,22 @@
 from django.urls import path
 from . import views
 from django.contrib.auth.views import LogoutView
+from .views import (
+    PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView,
+    home, register_view, CustomLoginView, profile_view
+)
+
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('register/', views.register_view, name='register'),
-    path('login/', views.CustomLoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
-    path('profile/', views.profile_view, name='profile'),
-    path('posts/', views.posts_view, name='posts'),
+    path('', home, name='home'),
+    path('posts/', PostListView.as_view(), name='posts'),
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('posts/new/', PostCreateView.as_view(), name='post-create'),
+    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
+    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+
+    path('register/', register_view, name='register'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('profile/', profile_view, name='profile'),
 ]
+
