2c8ae049f420a9dd6b226bfdf63b6c52a4292c00
otakukim <kimberlymjungwa@gmail.com>
2025-12-05 21:11:53 +0200

    Add Comment and User models to blog app

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -6,6 +6,18 @@ class Post(models.Model):
 	content: models.TextField()
 	published_date: models.DateTimeField(auto_now_add=True)
 	author: models.ForeignKey
+class Comment(models.Model):
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now, editable=False)
+    updated_at = models.DateTimeField(auto_now=True)
 
+class Meta:
+    ordering = ['created_at']
 def __str__(self):
+
     return self.title
+
+class User(models.Model):
+	author_name = models.CharField(max_length=200)
